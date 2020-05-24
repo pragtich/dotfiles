13986a407a047a9d84edb15dc131198f73068dcf
Joris Pragt <pragtich@gmail.com>
2020-05-24 10:11:17 +0200

    Install executables to .local/bin

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 shopt -s nullglob
 shopt -s dotglob
-GLOBIGNORE="install.sh:.git"
+GLOBIGNORE="install.sh:.git:bin"
 for f in *; do 
   echo "Synlinking $f"
-  ln -fs $PWD/$f ../$f
+  ln -fs "$PWD/$f" "../$f"
 done
+
+GLOBIGNORE=""
+mkdir -p ../.local/bin
+for f in bin/*; do
+  echo "Symlinking executable $f"
+  ln -fs "$PWD/$f" "../.local/$f"
+done	 
